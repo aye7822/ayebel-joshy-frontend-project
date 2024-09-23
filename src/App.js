89d2a564d87bc67d508c0a7ae384af2799a20cd1
HP <ayebel007@gmail.com>
2024-09-23 07:39:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutSection  from './components/AboutSection';
+import Projects from './components/Projects';
+import Services from './components/Services';
+import Team from './components/Team';
+import Testimonials from './components/Testimonials';
+import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
 import './App.css';
 
+// Main App component combining all other components
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <HeroSection />
+            <AboutSection/>
+            <Projects />
+            <Services />
+            <Team />
+            <Testimonials />
+            <ContactForm />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
